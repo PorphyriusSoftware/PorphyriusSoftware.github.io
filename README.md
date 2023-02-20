6b3ea10e07391efc2996df0a82f620f564536e58
Porfirio Mendez Ocampo <porfiriomendez76@gmail.com>
2023-02-20 01:30:45 -0700

    add exhaust particles and sound
    Added exhaust particles, fix some cursor hide and show, added sound for particles, added toggle sound switch

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # PorphyriusSoftware.github.io
 Portfolio
+
+Attributions:
+
+Game Image Resources:
+Credit "Kenney.nl"
+
+
+Game Sound Effects from:
+https://umipla.com/
--- index.js
+++ index.js
@@ -5,7 +5,6 @@
 const context = document.getElementById('backgroundCanvas').getContext('2d');
 context.canvas.width = window.innerWidth;
 context.canvas.height = window.innerHeight;
-console.log(context);
 
 
 class Game {
@@ -14,16 +13,25 @@ class Game {
     spaceship;
 
     /**
-     * Player is an image declared in the html.
+     * Player, playerLeft, playerRight is an image declared in the html.
      */
-    player = player;;
+    player = player;
+    playerLeft = playerLeft
+    playerRight = playerRight;
+    
+    /**
+     * particleBoop is a wav audio file declared in the html.
+     */
+    particleBoop = particleBoop;
+    spaceshipParticles = [];
+    lastTime = 0;
     constructor(ctx) {
         /**
          * Properties
          */
         this.ctx = ctx;
         this.mouse = { x: 0, y: 0 };
-        this.spaceship = new Spaceship(this.mouse, this.player);
+        this.spaceship = new Spaceship(this, this.mouse, this.player, this.playerLeft, this.playerRight, this.particleBoop);
 
         /**
          * Event listeners
@@ -39,9 +47,28 @@ class Game {
 
     }
 
+
     animate = (timeStamp) => {
+
+
+        let timeDifference = timeStamp - this.lastTime;
+        this.lastTime = timeStamp;
         this.ctx.clearRect(0, 0, this.ctx.canvas.width, this.ctx.canvas.height);
+
+        this.spaceshipParticles = this.spaceshipParticles
+            .filter((p) => {
+                return p.active;
+            });
+
+        this.spaceshipParticles
+            .forEach((p) => {
+                p.update(timeDifference);
+                p.draw(this.ctx);
+            });
+
+        this.spaceship.update(timeDifference);
         this.spaceship.draw(this.ctx);
+
         requestAnimationFrame(this.animate);
     }
 
@@ -59,26 +86,159 @@ class Mouse {
 }
 
 class Spaceship {
-
+    game;
     mouse;
     player;
-    constructor(mouse, player) {
+    playerLeft;
+    playerRight;
+    x;
+    y;
+    image;
+    timer = 0;
+    timerUpperLimit = 150;
+    particleBoop;
+    constructor(game, mouse, player, playerLeft, playerRight, particleBoop) {
+        this.game = game;
         this.mouse = mouse;
         this.player = player;
+        this.playerLeft = playerLeft;
+        this.playerRight = playerRight
+        this.image = player;//initial state;
+        this.particleBoop = particleBoop
     }
 
-    update = () => {
+
+    handleState = (state) => {
+        switch (state) {
+            case 'stopped':
+                this.image = this.player;
+                break;
+            case 'left':
+                this.image = this.playerLeft;
+                break;
+            case 'right':
+                this.image = this.playerRight;
+                break;
+            default:
+                this.image = this.player;
+                break;
+
+        }
+
+
+        let x = this.mouse.x;
+        let y = this.mouse.y + this.image.naturalHeight;
+        this.game.spaceshipParticles.push(new Particle(x, y, state, this.particleBoop));
+
 
     }
 
-    handleState = () => {
+    getMovingState() {
+        if (this.mouse.x > this.x) {
+            return 'right';
+        }
+
+        if (this.mouse.x < this.x) {
+            return 'left';
+        }
+
+        return 'stopped'
+    }
+
+    update = (timeDifference) => {
+        if (this.timer > this.timerUpperLimit) {
+            const state = this.getMovingState();
+            this.handleState(state);
+            this.timer = 0;
+        } else {
+            this.timer += timeDifference;
+        }
 
     }
+
 
 
     draw = (ctx) => {
         ctx.save();
-        ctx.drawImage(this.player, this.mouse.x - (this.player.naturalWidth * .5), this.mouse.y, 99, 75);
+        ctx.drawImage(this.image, this.mouse.x - (this.image.naturalWidth * .5), this.mouse.y, 99, 75);
+        ctx.restore();
+        /**
+         * capture the last X,Y position
+         */
+        this.x = this.mouse.x;
+        this.y = this.mouse.x;
+    }
+}
+
+class Particle {
+    x;
+    y;
+    state;
+    active = true;
+    size = 20;
+    timer = 0;
+    timerUpperLimit = 50;
+    color = 'white';
+
+    constructor(x, y, state, particleBoop) {
+        this.x = x;
+        this.y = y;
+        this.state = state;
+        try {
+            const shouldPlay = document.querySelector('#soundOn').checked;
+            if (shouldPlay) {
+                particleBoop.volume = 0.01;
+                particleBoop.play();
+                setTimeout(() => {
+                    particleBoop.pause();
+                    particleBoop.load();
+                }, 700);
+            }
+
+        } catch (e) {
+            /**
+            * should never get here
+            */
+            console.log(e);
+
+        }
+    }
+
+    update = (timeDifference) => {
+
+
+        if (this.timer > this.timerUpperLimit) {
+            this.y = this.y + 5;
+            this.size--;
+
+            if (this.size < 1) {
+                this.active = false;
+            }
+        } else {
+            this.timer += timeDifference;
+        }
+
+    }
+
+    getColor = () => {
+        switch (this.state) {
+            case 'stopped':
+                return 'white';
+            case 'left':
+                return 'red';
+            case 'right':
+                return 'blue'
+            default:
+                return 'yellow';
+        }
+    }
+
+    draw = (ctx) => {
+        ctx.save();
+        ctx.beginPath();
+        ctx.strokeStyle = this.getColor();
+        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        ctx.stroke();
         ctx.restore();
     }
 }
